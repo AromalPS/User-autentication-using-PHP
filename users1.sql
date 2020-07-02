8898d342a078272ce72a29c93760e3dd5415da08
AromalPS <19gce27@meaec.edu.in>
2020-07-02 15:48:57 +0530

    SQL table

--- users1.sql
+++ users1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2020 at 11:51 AM
+-- Generation Time: Jul 02, 2020 at 11:50 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.31
 
