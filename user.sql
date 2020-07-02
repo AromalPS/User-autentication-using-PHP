1633484b649f888d2ab4160bcac45d3ee8846779
AromalPS <19gce27@meaec.edu.in>
2020-07-02 15:29:53 +0530

    Added SQL

---
+++ user.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 02, 2020 at 11:51 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.2.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `user`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users1`
+--
+
+CREATE TABLE `users1` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `year` enum('1','2','3','4') DEFAULT NULL,
+  `college` varchar(30) DEFAULT NULL,
+  `dob` varchar(30) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users1`
+--
+
+INSERT INTO `users1` (`id`, `name`, `email`, `year`, `college`, `dob`, `password`) VALUES
+(1, 'Aromal P S', '19gce27@meaec.edu.in', '1', 'Mea Engineering college', '2001-11-28', '12345'),
+(2, 'salah', 'salah@gmail.com', '1', 'MES', '2006-04-10', 'salah'),
+(3, 'appu', 'appu@gmail.com', '3', 'KKKM', '1999-10-28', 'appu123'),
+(4, 'Achu', 'Achu@xyz.in', '3', 'TKM EG', '2000-03-01', 'achu1234'),
+(5, 'Ajmal', 'Ajmal@gmail.com', '1', 'Mea Engineering college', '2000-06-06', 'Ajmal123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users1`
+--
+ALTER TABLE `users1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users1`
+--
+ALTER TABLE `users1`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
